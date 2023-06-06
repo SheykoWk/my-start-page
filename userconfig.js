a9f9a802ed9cbe59524db5ab5712fcb1050ef671
Sh3yk0  (Sahid Kick) <78386975+SheykoWk@users.noreply.github.com>
2023-06-06 13:10:43 -0600

    Add files via upload

---
+++ userconfig.js
@@ -0,0 +1,209 @@
+const CONFIG = new Config({
+  crypto: {
+    coin: 'ETH',
+    currency: 'USD',
+    refreshIn: 10
+  },
+  overrideStorage: true, // override localStorage with fixed userconfig values
+  temperature: {
+    location: 'Guadalajara, Jalisco',
+    scale: 'C'
+  },
+  clock: {
+    format: 'h:i p',
+    iconColor: '#ff7b95'
+  },
+  search: {
+    engines: {
+      g: ['https://google.com/search?q=', 'Google'],
+      i: ['https://ixquick.com/do/search?q=', 'Ixquick'],
+      d: ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
+      y: ['https://youtube.com/results?search_query=', 'Youtube'],
+      w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
+    }
+  },
+  keybindings: {
+    "t": 'todo-list',
+    "s": 'search-bar'
+  },
+  disabled: ['crypto-rate'],
+  openLastVisitedTab: false,
+  tabs: [
+    {
+      name: 'Sh3yk0',
+      background_url: 'src/img/banners/bg-1.png',
+      categories: [{
+        name: 'Social Media',
+        links: [
+          {
+            url: 'https://www.facebook.com/',
+            name: 'Facebook',
+            icon: 'brand-facebook',
+            icon_color: '#2d86ff'
+          },
+          {
+            name: 'Youtube',
+            url: 'https://www.youtube.com/',
+            icon: 'brand-youtube',
+            icon_color: '#ff0000'
+          },
+          {
+            name: 'Twitter',
+            url: 'https://twitter.com/home',
+            icon: 'brand-twitter',
+            icon_color:'#1c9aef'
+          },
+          {
+            name: 'Instagram',
+            url: 'https://www.instagram.com/',
+            icon: 'brand-instagram',
+            icon_color: '#fc12a7'
+          },
+        ]
+      },
+      {
+        name: 'Messages',
+        links: [
+          {
+            icon: 'brand-whatsapp',
+            icon_color:'#0dc043',
+            url: 'https://web.whatsapp.com/'
+          },
+          {
+            icon: 'brand-telegram',
+            icon_color:'#3390ec',
+            url: 'https://web.telegram.org/k/'
+          },
+        ]
+      },
+      {
+        name: 'Technology',
+        links: [
+          {
+            name: '/g/',
+            url: 'https://4chan.org/g/',
+          },
+          {
+            name: '/Δ/',
+            url: 'https://archive.arisuchan.jp/%CE%94/catalog.html'
+          },
+          {
+            name: '/λ/',
+            url: 'https://www.lainchan.org/%CE%BB/catalog.html'
+          },
+          {
+            name: '/sec/',
+            url: 'https://www.lainchan.org/sec/catalog.html'
+          },
+          {
+            name: '/prog/',
+            url: 'https://boards.420chan.org/prog/',
+            icon: 'code'
+          }
+        ]
+      }
+      ]
+    },
+    {
+      name: 'music',
+      background_url: 'src/img/banners/bg-2.png',
+      categories: [
+        {
+          name: 'music services',
+          links: [
+            {
+              url: 'https://soundcloud.com/',
+              icon: 'brand-soundcloud',
+              icon_color: '#c57750'
+            },
+            {
+              url: 'https://youtu.be/',
+              icon: 'brand-youtube',
+              icon_color: '#996767'
+            },
+            {
+              url: 'https://r-a-d.io/',
+              icon: 'radio'
+            },
+          ]
+        },
+        {
+          name: 'music boards',
+          links: [
+            {
+              name: '/music/',
+              url: 'https://lainchan.org/music/',
+              icon: 'disc'
+            },
+            {
+              name: '/mu/',
+              url: 'https://boards.4channel.org/mu/',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'tech',
+      background_url: 'src/img/banners/bg-3.png',
+      categories: [
+        {
+          name: 'subreddits',
+          links: [
+            {
+              name: 'r/startpages/',
+              url: 'https://www.reddit.com/r/startpages/'
+            },
+            {
+              name: 'r/unixporn',
+              url: 'https://www.reddit.com/r/unixporn/'
+            },
+            {
+              name: 'r/mechkbds/',
+              url: 'https://www.reddit.com/r/MechanicalKeyboards/',
+              icon: 'keyboard',
+              icon_color: '#a57685'
+            },
+            {
+              name: 'r/programming',
+              url: 'https://www.reddit.com/r/programming/'
+            }
+          ]
+        },
+        {
+          name: 'blogs',
+          links: [
+            {
+              name: 'fasterthanli',
+              url: 'https://fasterthanli.me/articles',
+              icon: 'anchor'
+            },
+            {
+              name: 'dev.to',
+              url: 'https://dev.to'
+            },
+            {
+              name: 'mataroa.blog',
+              url: 'https://collection.mataroa.blog',
+            }
+          ]
+        },
+        {
+          name: 'misc',
+          links: [
+            {
+              name: 'post office',
+              url: 'http://afternoon.dynu.com/letterbox.html',
+              icon: 'mailbox'
+            },
+            {
+              name: 'rust docs',
+              url: 'https://doc.rust-lang.org/book/',
+              icon: 'notebook',
+              icon_color: '#977a3a'
+            }
+          ]
+        }
+      ]
+    }]
+});
